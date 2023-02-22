255e0d2a94515b5c43492a27a570547a3c73d54a
BoiseITGuru <dev@boiseitguru.com>
2023-02-22 09:47:13 -0700

    Blank App

---
+++ ella.wallet/ella_walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  ella_walletApp.swift
+//  ella.wallet
+//
+//  Created by BoiseITGuru on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ella_walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
